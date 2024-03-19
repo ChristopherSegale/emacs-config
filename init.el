6fe09fedfb9b89e7ccd94b390cab6a85d4bc9c67
ChristopherSegale <ccs.segale@gmail.com>
2024-03-18 23:12:11 -0400

    First commit

---
+++ init.el
@@ -0,0 +1,21 @@
+(setq backup-directory-alist 
+  '(("." . "~/.file-backups")))
+
+(add-to-list 'load-path "~/.emacs.d/use-package/")
+
+(require 'use-package)
+
+(use-package eat
+  :ensure t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(eat)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
